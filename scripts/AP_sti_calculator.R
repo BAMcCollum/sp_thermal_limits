8ef55058ca32c238e8b5e45bb73668440a3228f5
Breckie McCollum <bmccollum@berklee.edu>
2025-01-22 16:23:57 -0500

    update thermal pref figure

--- scripts/AP_sti_calculator.R
+++ scripts/AP_sti_calculator.R
@@ -352,28 +352,28 @@ read.csv(coefout,
          row.names = F)
 View(coefout)
 
+coefout <- read.csv("data/Occurrence_based_species_thermal_indicies_Photos_20250103.csv")
+
 # plot data ---------------------------------------------------------------
 
-color1 <- "#782391"
-color2 <- "#f2a23f"
+#color1 <- "#782391"
+color2 <- "#782391"
  coefout %>% 
   mutate(gen_spp = forcats::fct_reorder(gen_spp, BO21_tempmax_bdmean_mean) ) %>%
   
   ggplot(aes(x=gen_spp)) +
   
-  geom_point(aes(y=BO_sstmean_q5), color = color1, alpha=.5)+
-  geom_point(aes(y=BO_sstmean_q95),  color = color1, alpha=.5) +
-  geom_point(aes(y=BO_sstmean_mean),  color = color1, alpha=1, size=1.5 ) +
-  geom_segment(aes(xend=gen_spp,
-                   y=BO_sstmean_q5,
-                   yend=BO_sstmean_q95),  color = color1, alpha=.5) +
-  annotate(geom="text",
-           x=3, y=30, 
-           hjust=0, vjust=1,
-           label = "Mean SST",
-           color = color1,
-           fontface="bold",
-           size=5) +
+#  geom_point(aes(y=BO_sstmean_mean),  color = color1, alpha=1, size=1.5 ) +
+#  geom_segment(aes(xend=gen_spp,
+#                   y=BO_sstmean_q5,
+#                   yend=BO_sstmean_q95),  color = color1, alpha=.5) +
+#  annotate(geom="text",
+#           x=3, y=30, 
+#           hjust=0, vjust=1,
+#           label = "Mean SST",
+#           color = color1,
+#           fontface="bold",
+#           size=5) +
   
   geom_point(aes(y=BO21_tempmax_bdmean_q5),  color = color2, alpha=.5,
              position = position_nudge(x = 0.25))+
@@ -387,7 +387,7 @@ color2 <- "#f2a23f"
                position = position_nudge(x = 0.25)) +
   annotate(geom="text",
            x=3, y=30, 
-           hjust=0, vjust=1,
+           hjust=-0.1, vjust=0.2,
            label = 
              "\n Max temp at mean depth",
            color = color2,
@@ -395,10 +395,10 @@ color2 <- "#f2a23f"
            size=5) +
   
   
-  labs(x=NULL, y= "Water Temperature in C")+
+  labs(x=NULL, y= expression(paste("Water Temperature in ", "\u00b0C"))) +
    theme_bw() +
   theme(#plot.margin = margin(l=25,b=5,unit="pt"),
         axis.text.x = element_text(angle = -90, hjust = 0))
 
- ggsave("figures/thermal_tolerance_ranges.jpg")
+ ggsave("figures/thermal_preference_ranges.jpg")
 
