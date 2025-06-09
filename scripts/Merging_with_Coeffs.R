c6f91cfad53ec67231d807e81cb2ab4298fa25a7
Breckie McCollum <bmccollum@berklee.edu>
2025-06-09 15:37:41 -0400

    Added more potential figures

--- scripts/Merging_with_Coeffs.R
+++ scripts/Merging_with_Coeffs.R
@@ -10,7 +10,7 @@ library(taxize)
 setwd(here::here())
 #setwd("~/Dropbox (Byrnes Lab)/Breck_GOM/Data/R_Projects/sp_thermal_limits")
 
-thermal_indicies <- read.csv("data/Occurrence_based_species_thermal_indicies.csv")
+thermal_indicies <- read.csv("data/Occurrence_based_species_thermal_indicies_Photos_20250605.csv")
 View(thermal_indicies)
 
 # we should think more about which layers we are using, because there are loads
@@ -22,10 +22,11 @@ list_layers() %>% filter(str_detect(layer_code,"BO2_tempmean_bdmean")) %>% dplyr
 
 # plot data ---------------------------------------------------------------
 
+#MAX TEMP AT MEAN DEPTH + SST
 color1 <- "#782391"
 color2 <- "#f2a23f"
 
-thermal_indicies %>% 
+maxtemp_meandepthSST_plot <- thermal_indicies %>% 
   mutate(gen_spp = forcats::fct_reorder(gen_spp, BO21_tempmax_bdmean_mean) ) %>% # BO21... doesn't exist in thermal_indicies 
   
   ggplot(aes(x=gen_spp)) +
@@ -68,4 +69,104 @@ thermal_indicies %>%
   theme(#plot.margin = margin(l=25,b=5,unit="pt"),
     axis.text.x = element_text(angle = -90, hjust = 0))
 r
+
+#MAX TEMP AT MEAN DEPTH
+
+color2 <- "#f2a23f"
+
+maxtemp_meandepth_Pershing_plot <- thermal_indicies %>% 
+  mutate(gen_spp = forcats::fct_reorder(gen_spp, BO21_tempmax_bdmean_mean) ) %>% # BO21... doesn't exist in thermal_indicies 
+  
+  ggplot(aes(x=gen_spp)) +
+  
+  geom_point(aes(y=BO21_tempmax_bdmean_q5),  color = color2, alpha=.5,
+             position = position_nudge(x = 0.25))+
+  geom_point(aes(y=BO21_tempmax_bdmean_q95), color = color2, alpha=.5,
+             position = position_nudge(x = 0.25)) +
+  geom_point(aes(y=BO21_tempmax_bdmean_mean), color = color2, alpha=1, size=1.5,
+             position = position_nudge(x = 0.25)) +
+  geom_segment(aes(xend=gen_spp,
+                   y=BO21_tempmax_bdmean_q5,
+                   yend=BO21_tempmax_bdmean_q95), color = color2, alpha=.5,
+               position = position_nudge(x = 0.25)) +
+  annotate(geom="text",
+           x=3, y=30, 
+           hjust=0, vjust=1,
+           label = 
+             "\n Max temp at mean depth",
+           color = color2,
+           fontface="bold",
+           size=5) +
+  geom_hline(yintercept = 13.21, col = "turquoise")+
+  
+  labs(x=NULL, y= "Water Temperature in C")+
+  theme(#plot.margin = margin(l=25,b=5,unit="pt"),
+    axis.text.x = element_text(angle = -90, hjust = 0))
+
+#MAX TEMP AT MIN DEPTH (quantiles)
+
+color2 <- "#f2a23f"
+
+maxtemp_mindepth_Pershing_plotq <- thermal_indicies %>% 
+  mutate(gen_spp = forcats::fct_reorder(gen_spp, BO21_tempmax_bdmin_mean) ) %>% # BO21... doesn't exist in thermal_indicies 
+  
+  ggplot(aes(x=gen_spp)) +
+  
+  geom_point(aes(y=BO21_tempmax_bdmin_q5),  color = color2, alpha=.5,
+             position = position_nudge(x = 0.25))+
+  geom_point(aes(y=BO21_tempmax_bdmin_q95), color = color2, alpha=.5,
+             position = position_nudge(x = 0.25)) +
+  geom_point(aes(y=BO21_tempmax_bdmin_mean), color = color2, alpha=1, size=1.5,
+             position = position_nudge(x = 0.25)) +
+  geom_segment(aes(xend=gen_spp,
+                   y=BO21_tempmax_bdmin_q5,
+                   yend=BO21_tempmax_bdmin_q95), color = color2, alpha=.5,
+               position = position_nudge(x = 0.25)) +
+  annotate(geom="text",
+           x=3, y=30, 
+           hjust=0, vjust=1,
+           label = 
+             "\n Max temp at min depth",
+           color = color2,
+           fontface="bold",
+           size=5) +
+  
+  geom_hline(yintercept = 13.21, col = "turquoise")+
+  
+  labs(x=NULL, y= "Water Temperature in C")+
+  theme(#plot.margin = margin(l=25,b=5,unit="pt"),
+    axis.text.x = element_text(angle = -90, hjust = 0))
+
+#MAX TEMP AT MIN DEPTH (max and min)
+color1 <- "#782391"
+color2 <- "#f2a23f"
+
+maxtemp_mindepth_Pershing_plotminmax <- thermal_indicies %>% 
+  mutate(gen_spp = forcats::fct_reorder(gen_spp, BO21_tempmax_bdmin_mean) ) %>% # BO21... doesn't exist in thermal_indicies 
+  
+  ggplot(aes(x=gen_spp)) +
+  
+  geom_point(aes(y=BO21_tempmax_bdmin_min),  color = color2, alpha=.5)+
+  geom_point(aes(y=BO21_tempmax_bdmin_max), color = color2, alpha=.5) +
+  geom_point(aes(y=BO21_tempmax_bdmin_mean), color = color1, alpha=1, size=1.5) +
+  geom_segment(aes(xend=gen_spp,
+                   y=BO21_tempmax_bdmin_min,
+                   yend=BO21_tempmax_bdmin_max), color = color2, alpha=.5) +
+  annotate(geom="text",
+           x=2, y=32, 
+           hjust=0, vjust=0.2,
+           label = 
+             "\n Range of max temp at min depth",
+           color = color2,
+           fontface="bold",
+           size=5) +
+  
+  geom_hline(yintercept = 13.21, col = "turquoise")+
+  
+  labs(x=NULL, y= "Water Temperature in C")+
+  theme(#plot.margin = margin(l=25,b=5,unit="pt"),
+    axis.text.x = element_text(angle = -90, hjust = 0))
+
+ggsave("figures/maxtemp_mindepth_Pershing_plotminmax.jpg")
+
 
