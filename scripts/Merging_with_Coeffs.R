4e6afc78c96a00ae6bbd5d8b46e0581e880e937a
Breckie McCollum <bmccollum@berklee.edu>
2025-08-11 14:28:39 -0400

    updated figure, cleaned up script

--- scripts/Merging_with_Coeffs.R
+++ scripts/Merging_with_Coeffs.R
@@ -38,123 +38,8 @@ autofit(layers_table)
 
 # plot data ---------------------------------------------------------------
 
-#MAX TEMP AT MEAN DEPTH + SST
-color1 <- "#782391"
-color2 <- "#f2a23f"
-
-maxtemp_meandepthSST_plot <- thermal_indicies %>% 
-  mutate(gen_spp = forcats::fct_reorder(gen_spp, BO21_tempmax_bdmean_mean) ) %>% # BO21... doesn't exist in thermal_indicies 
-  
-  ggplot(aes(x=gen_spp)) +
-  
-  geom_point(aes(y=BO_sstmean_q5), color = color1, alpha=.5)+
-  geom_point(aes(y=BO_sstmean_q95),  color = color1, alpha=.5) +
-  geom_point(aes(y=BO_sstmean_mean),  color = color1, alpha=1, size=1.5 ) +
-  geom_segment(aes(xend=gen_spp,
-                   y=BO_sstmean_q5,
-                   yend=BO_sstmean_q95),  color = color1, alpha=.5) +
-  annotate(geom="text",
-           x=3, y=30, 
-           hjust=0, vjust=1,
-           label = "Mean SST",
-           color = color1,
-           fontface="bold",
-           size=5) +
-  
-  geom_point(aes(y=BO21_tempmax_bdmean_q5),  color = color2, alpha=.5,
-             position = position_nudge(x = 0.25))+
-  geom_point(aes(y=BO21_tempmax_bdmean_q95), color = color2, alpha=.5,
-             position = position_nudge(x = 0.25)) +
-  geom_point(aes(y=BO21_tempmax_bdmean_mean), color = color2, alpha=1, size=1.5,
-             position = position_nudge(x = 0.25)) +
-  geom_segment(aes(xend=gen_spp,
-                   y=BO21_tempmax_bdmean_q5,
-                   yend=BO21_tempmax_bdmean_q95), color = color2, alpha=.5,
-               position = position_nudge(x = 0.25)) +
-  annotate(geom="text",
-           x=3, y=30, 
-           hjust=0, vjust=1,
-           label = 
-             "\n Max temp at mean depth",
-           color = color2,
-           fontface="bold",
-           size=5) +
-  
-  
-  labs(x=NULL, y= "Water Temperature in C")+
-  theme(#plot.margin = margin(l=25,b=5,unit="pt"),
-    axis.text.x = element_text(angle = -90, hjust = 0))
-r
-
-#MAX TEMP AT MEAN DEPTH
-
-color2 <- "#f2a23f"
-
-maxtemp_meandepth_Pershing_plot <- thermal_indicies %>% 
-  mutate(gen_spp = forcats::fct_reorder(gen_spp, BO21_tempmax_bdmean_mean.x) ) %>% # BO21... doesn't exist in thermal_indicies 
-  
-  ggplot(aes(x=gen_spp)) +
-  
-  geom_point(aes(y=BO21_tempmax_bdmean_q5),  color = color2, alpha=.5,
-             position = position_nudge(x = 0.25))+
-  geom_point(aes(y=BO21_tempmax_bdmean_q95), color = color2, alpha=.5,
-             position = position_nudge(x = 0.25)) +
-  geom_point(aes(y=BO21_tempmax_bdmean_mean), color = color2, alpha=1, size=1.5,
-             position = position_nudge(x = 0.25)) +
-  geom_segment(aes(xend=gen_spp,
-                   y=BO21_tempmax_bdmean_q5,
-                   yend=BO21_tempmax_bdmean_q95), color = color2, alpha=.5,
-               position = position_nudge(x = 0.25)) +
-  annotate(geom="text",
-           x=3, y=30, 
-           hjust=0, vjust=1,
-           label = 
-             "\n Max temp at mean depth",
-           color = color2,
-           fontface="bold",
-           size=5) +
-  geom_hline(yintercept = 13.21, col = "turquoise")+
-  
-  labs(x=NULL, y= "Water Temperature in C")+
-  theme(#plot.margin = margin(l=25,b=5,unit="pt"),
-    axis.text.x = element_text(angle = -90, hjust = 0))
-
-#MAX TEMP AT MIN DEPTH (quantiles)
-
-color2 <- "#f2a23f"
-
-maxtemp_mindepth_Pershing_plotq <- thermal_indicies %>% 
-  mutate(gen_spp = forcats::fct_reorder(gen_spp, BO21_tempmax_bdmin_mean) ) %>% # BO21... doesn't exist in thermal_indicies 
-  
-  ggplot(aes(x=gen_spp)) +
-  
-  geom_point(aes(y=BO21_tempmax_bdmin_q5),  color = color2, alpha=.5,
-             position = position_nudge(x = 0.25))+
-  geom_point(aes(y=BO21_tempmax_bdmin_q95), color = color2, alpha=.5,
-             position = position_nudge(x = 0.25)) +
-  geom_point(aes(y=BO21_tempmax_bdmin_mean), color = color2, alpha=1, size=1.5,
-             position = position_nudge(x = 0.25)) +
-  geom_segment(aes(xend=gen_spp,
-                   y=BO21_tempmax_bdmin_q5,
-                   yend=BO21_tempmax_bdmin_q95), color = color2, alpha=.5,
-               position = position_nudge(x = 0.25)) +
-  annotate(geom="text",
-           x=3, y=30, 
-           hjust=0, vjust=1,
-           label = 
-             "\n Max temp at min depth",
-           color = color2,
-           fontface="bold",
-           size=5) +
-  
-  geom_hline(yintercept = 13.21, col = "turquoise")+
-  
-  labs(x=NULL, y= "Water Temperature in C")+
-  theme(#plot.margin = margin(l=25,b=5,unit="pt"),
-    axis.text.x = element_text(angle = -90, hjust = 0))
 
 #MAX TEMP AT MIN DEPTH (max and min)
-color1 <- "#782391"
 color2 <- "#f2a23f"
 
 maxtemp_mindepth_tipping_point_plotminmax <- thermal_indicies2 %>% 
@@ -162,13 +47,13 @@ maxtemp_mindepth_tipping_point_plotminmax <- thermal_indicies2 %>%
   
   ggplot(aes(x=gen_spp)) +
   
-  geom_point(aes(y=BO21_tempmax_bdmin_min.x),  color = color2, alpha = 1)+
-  geom_point(aes(y=BO21_tempmax_bdmin_max.x), color = color2, alpha = 1) +
+  geom_point(aes(y=BO21_tempmax_bdmin_min.x),  color = color2, alpha = 1, size = 3)+
+  geom_point(aes(y=BO21_tempmax_bdmin_max.x), color = color2, alpha = 1, size = 3) +
   geom_segment(aes(xend=gen_spp,
                    y=BO21_tempmax_bdmin_min.x,
-                   yend=BO21_tempmax_bdmin_max.x), color = color2, alpha=1) +
+                   yend=BO21_tempmax_bdmin_max.x), color = color2, alpha=1, linewidth = 1.5) +
   geom_point(aes(y=BO21_tempmax_bdmin_mean.x), alpha=1, size=3) +
-  theme_classic(base_size = 18)+
+  theme_classic(base_size = 20)+
   
  #annotate(geom="text",
 #           x=2, y=32, 
@@ -181,7 +66,7 @@ maxtemp_mindepth_tipping_point_plotminmax <- thermal_indicies2 %>%
   
   geom_hline(yintercept = 14.07, col = "turquoise", linewidth = 2)+
   geom_hline(yintercept = 17.4, col = "green", linewidth = 2)+
-  labs(x=NULL, y= "Water Temperature in C")+
+  labs(x=NULL, y = expression(paste("Water Temperature in ", "\u00b0C")))+
   theme(#plot.margin = margin(l=25,b=5,unit="pt"),
     axis.text.x = element_text(angle = -90, hjust = 0, face="italic"))
 
